c75e2750391a43b56501027208f1487d1487f076
fengwu2004 <user@users-imac-2.local>
2015-11-16 16:44:14 +0800

    Initial Commit

---
+++ MyTextView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyTextView
+//
+//  Created by user on 15/11/16.
+//  Copyright © 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
